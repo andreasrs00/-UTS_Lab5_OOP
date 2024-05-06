b7c38d5568e9b7de3c39c2f7600009ed0d89e1fd
Andreas Robson Simanjuntak <xandersimanjuntak@gmail.com>
2024-05-06 22:57:28 +0700

    first(3classno1)

---
+++ src/No1/Activity.java
@@ -0,0 +1,19 @@
+package No1;
+
+public class Activity {
+    private String activityName;
+
+    public Activity(String activityName){
+        this.activityName = activityName;
+    }
+
+//    getter n setter
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+}
---
+++ src/No1/PhysicalActivity.java
@@ -0,0 +1,34 @@
+package No1;
+
+public class PhysicalActivity extends Activity {
+    private int duration;
+    private double caloriesBurned;
+
+    public PhysicalActivity(String activityName, int duration, double caloriesBurned){
+        super(activityName);
+        this.duration = duration;
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    //method overloading to record physical activity with different parameters
+    public void addActivity(int duration){
+        this.duration = duration;
+    }
+
+    public void addActivity(int duration, double caloriesBurned){
+        this.duration = duration;
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    // method to calculate calories burned
+    public double calculateCaloriesBurned(double weight) {
+        // simple callculation
+        return (caloriesBurned * duration)/ weight;
+    }
+
+    // method overriding to calculate calories burned considering height n weight
+    public double calculateCaloriesBurned(double weight, int height){
+        // extending functionally by considering height
+        return ((caloriesBurned * duration) / weight) * height;
+    }
+}
